c92a6291233a79d8728ff19553e3d6b51160dae2
Gael Mayen <oskarinmate@gmail.com>
2026-01-12 17:17:36 -0600

    Argumentos variables y ciclo ForEach

---
+++ src/ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int edades [] = {5, 10, 30, 45};
+        for(int edad: edades ){
+            System.out.println("edad = " + edad);
+        }
+    }
+}
