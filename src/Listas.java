ee653086e4bcca849f10e88c8c2e7ae16a73c66c
Gael Mayen <oskarinmate@gmail.com>
2026-01-15 16:24:00 -0600

    Listas Arrays.asList en Java

--- src/Listas.java
+++ src/Listas.java
@@ -1,28 +1,36 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 
 public class Listas {
     public static void main(String[] args) {
 
         // ArrayList permite elementos duplicados y tiene un orden
-        List<String>miLista = new ArrayList<>();
-        miLista.add("Lunes");
-        miLista.add("Martes");
-        miLista.add("Miercoles");
-        miLista.add("Jueves");
-        miLista.add("Viernes");
-        miLista.add("Sabado");
-        miLista.add("Domingo");
-        miLista.add("Esto es un String");
+//        List<String>miLista = new ArrayList<>();
+//        miLista.add("Lunes");
+//        miLista.add("Martes");
+//        miLista.add("Miercoles");
+//        miLista.add("Jueves");
+//        miLista.add("Viernes");
+//        miLista.add("Sabado");
+//        miLista.add("Domingo");
+//        miLista.add("Esto es un String");
 
 
-        for(String elemento : miLista ){
-            System.out.println("Dia de la semana: " + elemento);
-        }
+//        for(String elemento : miLista ){
+//            System.out.println("Dia de la semana: " + elemento);
+//        }
 
-        // Funciones Lambda funcion anonima de un codigo muy compacto
-        miLista.forEach(elemento -> {
-            System.out.println("Elemento: " + elemento);
-        });
+//        // Funciones Lambda funcion anonima de un codigo muy compacto
+//        miLista.forEach(elemento -> {
+//            System.out.println("Elemento: " + elemento);
+//        });
+
+//        miLista.forEach(System.out::println);
+
+        List<String> nombres = Arrays.asList("Pedro" , "Ivonne" , "Nohemi");
+        nombres.forEach(System.out::println);
+
     }
 }
