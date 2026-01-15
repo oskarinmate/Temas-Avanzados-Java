468527c70e0003a142ee7af5d82e1832f6343ef8
Gael Mayen <oskarinmate@gmail.com>
2026-01-15 15:36:22 -0600

    Listas ArrayList en Java

---
+++ src/Listas.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Listas {
+    public static void main(String[] args) {
+
+        // ArrayList permite elementos duplicados y tiene un orden
+        List miLista = new ArrayList<>();
+        miLista.add("Lunes");
+        miLista.add("Martes");
+        miLista.add("Miercoles");
+        miLista.add("Jueves");
+        miLista.add("Viernes");
+        miLista.add("Sabado");
+        miLista.add("Domingo");
+        miLista.add("Domingo");
+
+        for(Object elemento : miLista ){
+            System.out.println("Dia de la semana: " + elemento);
+        }
+
+    }
+}
